91dc64ced0323e9b0884e6a7e3f4980589916cdd
Mustafa <mustafabeyazbulut@unvergroup.com>
2022-03-05 15:16:20 +0300

    .

---
+++ QRbarkodolustur/QRbarkodolustur/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace QRbarkodolustur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.yazicidi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(504, 214);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 20);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(386, 237);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(480, 140);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "F14-105446-00%07%2111262";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(406, 214);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 43);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Yazdır";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // yazicidi
+            // 
+            this.yazicidi.Location = new System.Drawing.Point(480, 175);
+            this.yazicidi.Margin = new System.Windows.Forms.Padding(2);
+            this.yazicidi.Name = "yazicidi";
+            this.yazicidi.Size = new System.Drawing.Size(122, 20);
+            this.yazicidi.TabIndex = 4;
+            this.yazicidi.Text = "Pica II";
+            this.yazicidi.TextChanged += new System.EventHandler(this.yazicidi_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(400, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Basılacak Kod";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(403, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Yazıcı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(400, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Dinamo Kod";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(472, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(214, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(403, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Adet";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(472, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 283);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.yazicidi);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox yazicidi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ QRbarkodolustur/QRbarkodolustur/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace QRbarkodolustur
+{
+    public partial class Form1 : Form
+    {
+        List<Bitmap> bmps;
+        int index = 0;
+        int genislik = 0;
+        int yukseklik = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(textBox1.Text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            
+            Graphics graphicsImage = Graphics.FromImage(qrCodeImage);
+            string Str_TextOnImage = "F14-105446-00-07";
+            StringFormat stringformat = new StringFormat();
+
+            Color StringColor = System.Drawing.Color.Black;//direct color adding  
+            graphicsImage.DrawString(Str_TextOnImage, new Font("arial", 40,
+            FontStyle.Regular), new SolidBrush(StringColor), new Point(qrCodeImage.Width / 5, (qrCodeImage.Height-(qrCodeImage.Height/10))),
+            stringformat);
+
+
+            pictureBox1.Image = qrCodeImage;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bmps = new List<Bitmap>();
+
+                if (textBox2.Text == "")
+                {
+                    MessageBox.Show("Adet Bilgisi girişi yapınız.");
+                    return;
+                }
+
+                int index1 = Convert.ToInt32(textBox2.Text);
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    string yenikod = textBox1.Text + "|" + Convert.ToString(i).PadLeft(6, '0');
+                    QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                    QRCodeData qrCodeData = qrGenerator.CreateQrCode(yenikod, QRCodeGenerator.ECCLevel.Q);
+                    QRCode qrCode = new QRCode(qrCodeData);
+                    Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+                    Graphics graphicsImage = Graphics.FromImage(qrCodeImage);
+                    string Str_TextOnImage = "F14-105446-00-07";
+                    StringFormat stringformat = new StringFormat();
+
+                    Color StringColor = System.Drawing.Color.Black;//direct color adding  
+                    graphicsImage.DrawString(Str_TextOnImage, new Font("arial", 45,
+                    FontStyle.Bold), new SolidBrush(StringColor), new Point(qrCodeImage.Width / 6, (qrCodeImage.Height - (qrCodeImage.Height / 10))),
+                    stringformat);
+                    Bitmap bmp = new Bitmap(qrCodeImage.Width, qrCodeImage.Height, graphicsImage);
+
+                    bmps.Add(qrCodeImage);
+                }
+
+
+
+                //bmps.Add(qrCodeImage);
+
+                /*
+                genislik = qrCodeImage.Width;
+                yukseklik = qrCodeImage.Height;
+                MessageBox.Show(genislik.ToString()+"*"+yukseklik.ToString());
+                */
+                genislik = 60;
+                yukseklik = 60;
+
+                PrintDocument pd = new PrintDocument();
+
+                pd.PrinterSettings.PrinterName = yazicidi.Text;
+
+                pd.PrinterSettings.DefaultPageSettings.PaperSize = new PaperSize("Card", genislik, yukseklik);
+                pd.DefaultPageSettings.Landscape = true;
+                pd.DefaultPageSettings.PrinterResolution = new PrinterResolution
+                {
+                    Kind = PrinterResolutionKind.Custom,
+                    X = 300,
+                    Y = 300
+                };
+
+                pd.DefaultPageSettings.Margins = new Margins(2, 2, 2, 2);
+
+                pd.PrintPage += new PrintPageEventHandler(imprimirDocumento);
+
+
+                pd.Print();
+                //PrintPreviewDialog previewDialog = new PrintPreviewDialog();
+                //previewDialog.Document = pd;
+                //previewDialog.ShowDialog();
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void imprimirDocumento(System.Object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawImage(bmps[index++], 0, 0, genislik, yukseklik);
+            if (index < bmps.Count) e.HasMorePages = true; else index = 0;
+        }
+
+        private void yazicidi_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        String Con = "Provider=SQLNCLI10;Server=10.0.1.13; Database=UNVAS_2022; Uid=sa; Pwd=1q2w3e4r--";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            String sql_query = "SELECT * FROM UFLD02E WITH(NOLOCK) WHERE TABLE1='STOK00' AND QS_VARCODE='MKODU' AND QS_VALUE<>'' ORDER BY KRITER";
+
+            string sqlWorkorderModel = Con;
+            SqlDataAdapter sqlDataAdapter;
+            OleDbDataAdapter workOrdAdap, mainStockAdap, woDetailAdap, additionalStockAdap;
+            DataTable dataWorkOrderModel, dataMainStockModel, dataWoDetailModel, dataAdditionalStockModel, dataTrexToDinamo;
+            OleDbCommand oleDbCommand;
+            OleDbConnection oleDbConnection;
+
+            oleDbConnection = new OleDbConnection(sqlWorkorderModel);
+            oleDbConnection.Open();
+
+            workOrdAdap = new OleDbDataAdapter(sql_query, oleDbConnection);
+            dataWorkOrderModel = new DataTable();
+            workOrdAdap.Fill(dataWorkOrderModel);
+            // MessageBox.Show(dataWorkOrderModel.Rows.Count.ToString());
+            ComboboxItem item = new ComboboxItem();
+
+            for (int j = 0; j < dataWorkOrderModel.Rows.Count; j++)
+            {
+
+                //item.Text = dataWorkOrderModel.Rows[j]["KRITER"].ToString().Trim();
+                //item.Value = dataWorkOrderModel.Rows[j]["QS_VALUE"].ToString().Trim();
+                //comboBox1.Items.Add(item);
+                comboBox1.Items.Add(dataWorkOrderModel.Rows[j]["KRITER"].ToString().Trim());
+
+            }
+            this.Controls.Add(comboBox1);
+            comboBox1.SelectedIndex = 0;
+        }
+
+        public class ComboboxItem
+        {
+            public string Text { get; set; }
+            public string Value { get; set; }
+
+            public override string ToString()
+            {
+                return Text;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string selected = this.comboBox1.GetItemText(this.comboBox1.SelectedItem);
+           // MessageBox.Show(selected);
+
+
+            //ComboBox cmb = (ComboBox)sender;
+            //int selectedIndex = cmb.SelectedIndex;
+            //string selectedValue = (string)cmb.SelectedValue;
+            //ComboboxItem selectedCar = (ComboboxItem)cmb.SelectedItem;
+            //MessageBox.Show(String.Format("Index: [{0}] CarName={1}; Value={2}", selectedIndex, selectedCar.Text, selectedCar.Value));
+
+            string sql = "SELECT QS_VALUE FROM UFLD02E WITH(NOLOCK) WHERE TABLE1='STOK00' AND QS_VARCODE='MKODU' AND QS_VALUE<>'' AND KRITER='" + selected + "'";
+
+            string sqlWorkorderModel = Con;
+            OleDbDataAdapter workOrdAdap;
+            DataTable dataWorkOrderModel;
+            OleDbConnection oleDbConnection;
+
+            oleDbConnection = new OleDbConnection(sqlWorkorderModel);
+            oleDbConnection.Open();
+
+            workOrdAdap = new OleDbDataAdapter(sql, oleDbConnection);
+            dataWorkOrderModel = new DataTable();
+            workOrdAdap.Fill(dataWorkOrderModel);
+
+            
+            if (dataWorkOrderModel.Rows.Count > 0 )
+            { 
+                textBox1.Text =  dataWorkOrderModel.Rows[0]["QS_VALUE"].ToString().Trim();
+            }
+
+            
+
+            //int selectedIndex = comboBox1.SelectedIndex;
+            //comboBox1.SelectedItem.ToString();
+            //string selectedValue = comboBox1.Items[selectedIndex].ToString();
+            // ComboBox cmb = (ComboBox)sender;
+            //int selectedIndex = cmb.SelectedIndex;
+            //string selectedText = this.comboBox1.Text;
+            //string selectedValue = ((ComboboxItem)comboBox1.SelectedItem).Value.ToString();
+            //string selectedValue01 = comboBox1.Items.ToString();
+            //MessageBox.Show(comboBox1.SelectedValue.ToString());
+            //   textBox1.Text = selectedValue;// comboBox1.SelectedItem.ToString();
+            //textBox1.Text = comboBox1.SelectedValue.ToString();
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+    }
+}
